aa820b76548d232e1f0a422fee483c63f1d3193f
Marc-André Lemay <marc-andre.lemay.2@ulaval.ca>
2021-05-26 11:27:07 -0400

    Integrate the code for Figures S13 and S14 into Makefile

--- Makefile
+++ Makefile
@@ -82,6 +82,10 @@ figures/figure_s11.png : nanoplot_stats/N50_stats.txt \
 
 figures/figure_s12.png : sv_genotyping/nanopore_svs/sveval_benchmarks/norepeat_RData/sveval_norepeat_rates.RData scripts/make_plot_data.R
 
+figures/figure_s13.png : sv_genotyping/nanopore_svs/sveval_benchmarks/nogeno_RData/sveval_nogeno_rates.RData scripts/make_plot_data.R
+
+figures/figure_s14.png : sv_genotyping/nanopore_svs/sveval_benchmarks/norepeat_RData/sveval_norepeat_rates.RData scripts/make_plot_data.R
+
 tables/table_s1.csv tables/table_s2.csv tables/table_s3.csv: tables/formatting_sup_tables.R
 	cd tables; $(R_RUN_COMMAND) formatting_sup_tables.R
 
--- figures/figure_s13.R
+++ figures/figure_s13.R
@@ -1,4 +1,4 @@
-#!/prg/R/4.0/bin/Rscript
+#!/usr/bin/Rscript
 
 # Figure S13 shows the benchmarking of duplications and inversions discovered using Oxford Nanopore sequencing
 #  and genotyped using the Illumina sequencing data
@@ -9,12 +9,12 @@ library(ggplot2)
 library(grid)
 
 # Loading the data used for plotting
-# DEPENDENCY : sveval_nogeno_rates.RData
-load("/home/malem420/WGS_data/bbduk_trimmed/bwa_alignment_Gmax_v4/paragraph_nanopore/genotyping/svmerged/paragraph/sveval_benchmarks/nogeno_RData/sveval_nogeno_rates.RData")
+# DEPENDENCY : sv_genotyping/nanopore_svs/sveval_benchmarks/nogeno_RData/sveval_nogeno_rates.RData
+load("../sv_genotyping/nanopore_svs/sveval_benchmarks/nogeno_RData/sveval_nogeno_rates.RData")
 
 # Also loading a script that will be used to prepare the data for plotting
-# DEPENDENCY : make_plot_data.R
-source("/home/malem420/scripts/make_plot_data.R")
+# DEPENDENCY : scripts/make_plot_data.R
+source("../scripts/make_plot_data.R")
 
 # Preparing the data for plotting
 dup_plot_data <- make_plot_data(sveval_nogeno_rates, "DUP")
@@ -60,6 +60,7 @@ inversions_plot <-
 
 
 # Saving to disk as "figure_s13.png"
+# OUTPUT : figures/figure_s13.png
 png("figure_s13.png", width = 3, height = 6, units = "in", res = 500)
 grid.newpage()
 # Locating the subplots in the figure
